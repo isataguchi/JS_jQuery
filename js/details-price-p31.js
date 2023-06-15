8a01e53671c2cf409266c938d320e87b321a58bf
isataguchi <122779306+isataguchi@users.noreply.github.com>
2023-06-15 19:16:55 +0900

    JS and JQuery Assignment
    Learning about JS and JQuery

--- js/details-price-p31.js
+++ js/details-price-p31.js
@@ -1,124 +1,121 @@
-$(document).ready(function() {
-    $('.qtd-p31').on('input', function() {
-      var quantity31 = $(this).val();
-      $('.qtd-p31-display').text(quantity31);
-      $(this).siblings('.qtd-p31-display').text(quantity31);
-    });
+$(document).ready(function () {
+  $(".qtd-p31").on("input", function () {
+    var quantity31 = $(this).val();
+    $(".qtd-p31-display").text(quantity31);
+    $(this).siblings(".qtd-p31-display").text(quantity31);
   });
-  $(document).ready(function() {
-    $('.qtd-p32').on('input', function() {
-      var quantity32 = $(this).val();
-      $('.qtd-p32-display').text(quantity32);
-      $(this).siblings('.qtd-p32-display').text(quantity32);
-    });
+});
+$(document).ready(function () {
+  $(".qtd-p32").on("input", function () {
+    var quantity32 = $(this).val();
+    $(".qtd-p32-display").text(quantity32);
+    $(this).siblings(".qtd-p32-display").text(quantity32);
   });
-  
-  $(document).ready(function() {
-    $('.qtd-p33').on('input', function() {
-      var quantity33 = $(this).val();
-      $('.qtd-p33-display').text(quantity33);
-      $(this).siblings('.qtd-p33-display').text(quantity33);
-    });
+});
+
+$(document).ready(function () {
+  $(".qtd-p33").on("input", function () {
+    var quantity33 = $(this).val();
+    $(".qtd-p33-display").text(quantity33);
+    $(this).siblings(".qtd-p33-display").text(quantity33);
   });
-  
-$(document).ready(function() {
-    $('#prc-p31, #qtd-p31').on('click', function() {
-        calculateTotalPrice();
-    });
+});
 
-      function calculateTotalPrice() {
-       
-        var price31 = parseFloat($('.prc-p31').text().trim());
-        var quantity31 = parseInt($('.qtd-p31').val());
-    
-        // Check if the price and quantity are valid numbers
-        if (!isNaN(price31) && !isNaN(quantity31)) {
-          // Calculate the total price
-          var subtotal31 = price * quantity31;
-        var tax = subtotal31 * 0.1;
-          var total31 = subtotal + tax;
-    
-          // Display the total price in the appropriate element
-          $('#total-p31').text('$' + total31.toFixed(2));
-        }
-      }
-    });
+$(document).ready(function () {
+  $("#prc-p31, #qtd-p31").on("click", function () {
+    calculateTotalPrice();
+  });
 
-    $(document).ready(function() {
-        $('#prc-p32, #qtd-p32').on('click', function() {
-            calculateTotalPrice();
-        });
-    
-          function calculateTotalPrice() {
-           
-            var price32 = parseFloat($('.prc-p32').text().trim());
-            var quantity32 = parseInt($('.qtd-p32').val());
-        
-            // Check if the price and quantity are valid numbers
-            if (!isNaN(price32) && !isNaN(quantity32)) {
-              // Calculate the total price
-              var subtotal32 = price32 * quantity32;
-            var tax = subtotal32 * 0.1;
-              var total32 = subtotal32 + tax;
-        
-              // Display the total price in the appropriate element
-              $('#total-p32').text('$' + total32.toFixed(2));
-            }
-          }
-        });
-        $(document).ready(function() {
-            $('#prc-p33, #qtd-p33').on('click', function() {
-                calculateTotalPrice();
-            });
-        
-              function calculateTotalPrice() {
-               
-                var price33 = parseFloat($('.prc-p33').text().trim());
-                var quantity33 = parseInt($('.qtd-p33').val());
-            
-                // Check if the price and quantity are valid numbers
-                if (!isNaN(price33) && !isNaN(quantity33)) {
-                  // Calculate the total price
-                  var subtotal33 = price33 * quantity33;
-                var tax = subtotal33 * 0.1;
-                  var total33 = subtotal33 + tax;
-            
-                  // Display the total price in the appropriate element
-                  $('#total-p33').text('$' + total33.toFixed(2));
-                }
-              }
-            });
-            $(document).ready(function() {
-                $('.qtd-p31, .qtd-p32, .qtd-p33').on('input', function() {
-                  calculateTotalPrice();
-                });
-              
-                function calculateTotalPrice() {
-                  var price31 = parseFloat($('#prc-p31').text().trim());
-                  var quantity31 = parseInt($('.qtd-p31').val()) || 0;
-                  var price32 = parseFloat($('#prc-p32').text().trim());
-                  var quantity32 = parseInt($('.qtd-p32').val()) || 0;
-                  var price33 = parseFloat($('#prc-p33').text().trim());
-                  var quantity33 = parseInt($('.qtd-p33').val()) || 0;
-              
-                  var taxRate = 0.1; // 10% tax rate
+  function calculateTotalPrice() {
+    var price31 = parseFloat($(".prc-p31").text().trim());
+    var quantity31 = parseInt($(".qtd-p31").val());
 
-                  // Calculate the total prices for each product
-                  var total31 = price31 * quantity31 * (1 + taxRate);
-                  var total32 = price32 * quantity32 * (1 + taxRate);
-                  var total33 = price33 * quantity33 * (1 + taxRate);
-              
-                  // Display the total prices in the appropriate elements
-                  $('.total-p31').text('$' + total31.toFixed(2));
-                  $('.total-p32').text('$' + total32.toFixed(2));
-                  $('.total-p33').text('$' + total33.toFixed(2));
-              
-                  // Calculate the total quantity
-                  var totalQuantity = quantity31 + quantity32 + quantity33;
-                  $('#total-quantity-p20').text(totalQuantity);
-              
-                  // Calculate the total price
-                  var totalPrice = total31 + total32 + total33;
-                  $('#total-price-p20').text('$' + totalPrice.toFixed(2));
-                }
-              });
+    // Check if the price and quantity are valid numbers
+    if (!isNaN(price31) && !isNaN(quantity31)) {
+      // Calculate the total price
+      var subtotal31 = price * quantity31;
+      var tax = subtotal31 * 0.1;
+      var total31 = subtotal + tax;
+
+      // Display the total price in the appropriate element
+      $("#total-p31").text("$" + total31.toFixed(2));
+    }
+  }
+});
+
+$(document).ready(function () {
+  $("#prc-p32, #qtd-p32").on("click", function () {
+    calculateTotalPrice();
+  });
+
+  function calculateTotalPrice() {
+    var price32 = parseFloat($(".prc-p32").text().trim());
+    var quantity32 = parseInt($(".qtd-p32").val());
+
+    // Check if the price and quantity are valid numbers
+    if (!isNaN(price32) && !isNaN(quantity32)) {
+      // Calculate the total price
+      var subtotal32 = price32 * quantity32;
+      var tax = subtotal32 * 0.1;
+      var total32 = subtotal32 + tax;
+
+      // Display the total price in the appropriate element
+      $("#total-p32").text("$" + total32.toFixed(2));
+    }
+  }
+});
+$(document).ready(function () {
+  $("#prc-p33, #qtd-p33").on("click", function () {
+    calculateTotalPrice();
+  });
+
+  function calculateTotalPrice() {
+    var price33 = parseFloat($(".prc-p33").text().trim());
+    var quantity33 = parseInt($(".qtd-p33").val());
+
+    // Check if the price and quantity are valid numbers
+    if (!isNaN(price33) && !isNaN(quantity33)) {
+      // Calculate the total price
+      var subtotal33 = price33 * quantity33;
+      var tax = subtotal33 * 0.1;
+      var total33 = subtotal33 + tax;
+
+      // Display the total price in the appropriate element
+      $("#total-p33").text("$" + total33.toFixed(2));
+    }
+  }
+});
+$(document).ready(function () {
+  $(".qtd-p31, .qtd-p32, .qtd-p33").on("input", function () {
+    calculateTotalPrice();
+  });
+
+  function calculateTotalPrice() {
+    var price31 = parseFloat($("#prc-p31").text().trim());
+    var quantity31 = parseInt($(".qtd-p31").val()) || 0;
+    var price32 = parseFloat($("#prc-p32").text().trim());
+    var quantity32 = parseInt($(".qtd-p32").val()) || 0;
+    var price33 = parseFloat($("#prc-p33").text().trim());
+    var quantity33 = parseInt($(".qtd-p33").val()) || 0;
+
+    var taxRate = 0.1; // 10% tax rate
+
+    // Calculate the total prices for each product
+    var total31 = price31 * quantity31 * (1 + taxRate);
+    var total32 = price32 * quantity32 * (1 + taxRate);
+    var total33 = price33 * quantity33 * (1 + taxRate);
+
+    // Display the total prices in the appropriate elements
+    $(".total-p31").text("$" + total31.toFixed(2));
+    $(".total-p32").text("$" + total32.toFixed(2));
+    $(".total-p33").text("$" + total33.toFixed(2));
+
+    // Calculate the total quantity
+    var totalQuantity = quantity31 + quantity32 + quantity33;
+    $("#total-quantity-p30").text(totalQuantity);
+
+    // Calculate the total price
+    var totalPrice = total31 + total32 + total33;
+    $("#total-price-p30").text("$" + totalPrice.toFixed(2));
+  }
+});
--- js/validateform.js
+++ js/validateform.js
@@ -1,18 +1,78 @@
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
 function validateForm() {
-    var requiredFields = document.querySelectorAll('input[required]');
-  
-    for (var i = 0; i < requiredFields.length; i++) {
-      if (requiredFields[i].value === '') {
-        alert('Please fill in all required fields.');
-        return false; // Prevent form submission
+  var requiredFields = document.querySelectorAll("input[required]");
+
+  var hasEmptyFields = false; // Flag to track if there are empty required fields
+
+  requiredFields.forEach(function (input) {
+    if (input.value === "") {
+      input.classList.add("error");
+      hasEmptyFields = true;
+    } else {
+      input.classList.remove("error");
+    }
+  });
+  if (hasEmptyFields) {
+    alert("Please fill in all required fields.");
+    return false; // Prevent form submission
+  }
+
+  // Ask for confirmation before submitting the form
+  var confirmation = confirm("Are you sure you want to submit the form?");
+  if (!confirmation) {
+    return false; // Prevent form submission
+  }
+
+  // Payment completion handling
+  console.log("Payment completed. Redirecting to thank you page...");
+
+  // Redirect to the thank you page
+  window.location.href = "./thankyou.html";
+
+  return true; // Allow form submission
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const creditCardInput = document.getElementById("creditCardInput");
+
+  creditCardInput.addEventListener("input", formatCreditCardNumber);
+
+  function formatCreditCardNumber() {
+    let inputValue = this.value;
+
+    // Remove any non-numeric characters
+    inputValue = inputValue.replace(/[^0-9]/g, "");
+
+    // Limit to 16 characters
+    inputValue = inputValue.slice(0, 16);
+
+    // Group the digits into sets of four with a space
+    inputValue = inputValue.replace(/(\d{4})(?=\d)/g, "$1 ");
+
+    // Update the input value
+    this.value = inputValue;
+  }
+
+  creditCardInput.addEventListener("keyup", function (e) {
+    if (e.keyCode === 13) {
+      validateForm(); // Enter key pressed, call the validateForm function
+      return false;
+    } else {
+      if (creditCardInput.value == null) {
+        creditCardInput.type = "text";
+        // Input is empty, convert to 'text' to prevent password autofill
+      } else {
+        creditCardInput.type = "password";
+        // Input is not empty, convert to 'password' to hide text entry
       }
     }
-  
-    // Ask for confirmation before submitting the form
-    var confirmation = confirm('Are you sure you want to submit the form?');
-    if (!confirmation) {
-      return false; // Prevent form submission
-    }
-  
-    return true; // Allow form submission
-  }
+  });
+});
